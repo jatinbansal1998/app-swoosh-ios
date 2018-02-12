90a9014e19a076ec7388c1160d3622163f2cd66f
Jatin Bansal <jatinbansal1998@Jatins-MacBook-Pro.local>
2018-02-12 17:45:53 +0530

    iOS UI design and custom UI class

---
+++ Swoosh/Swoosh/Views/BorderButton.swift
@@ -0,0 +1,26 @@
+//
+//  BorderButton.swift
+//  Swoosh
+//
+//  Created by Jatin Bansal on 12/02/18.
+//  Copyright © 2018 Jatin Bansal. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.borderWidth = 2.0
+        layer.borderColor = UIColor.white.cgColor
+    }
+
+}
